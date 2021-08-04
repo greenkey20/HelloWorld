e7be4e5ec33ab0f0c9481e60531554fdca073654
Eunyoung KANG <greenpianorabbit@localhost>
2021-08-04 17:05:40 +0900

    initial commit

---
+++ HelloWorld1/src/HelloWorld1.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I am Eunyoung");
+		System.out.println("It is the 4th of August 2021");
+	}	
+}
